29288167f506d6a9b94ffd1ea4950bf8903b8d66
JamesNg <njames15@gmail.com>
2022-05-01 21:08:01 -0400

    commit tree bb1de40b3a2b2dfe533b6157de9fefe6ec10c3d9

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 function addingEventListener() {
+    const input = document.querySelector('#input');
+    function clickAlert() {
+        alert('I was clicked!');
+    }
+    input.addEventListener('click', clickAlert);
 }
